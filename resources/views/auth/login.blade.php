5e11ddfc0f3b393c8ff59faac6c9c8cd24afab79
Raul Villanueva III <106396891+Villanueva0909@users.noreply.github.com>
2025-12-12 12:07:09 +0800

    Making Auth Controller

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="flex flex-col justify-center sm:h-screen p-4">
+        <div class="max-w-md w-full mx-auto border border-gray-300 rounded-2xl p-8">
+            <div class="text-center mb-12">
+                <a href="javascript:void(0)"><img
+                        src="https://readymadeui.com/readymadeui.svg" alt="logo" class="w-40 inline-block" />
+                </a>
+            </div>
+
+            <form>
+                <div class="space-y-6">
+                    <div>
+                        <label class="text-slate-900 text-sm font-medium mb-2 block">Email Id</label>
+                        <input name="email" type="text" class="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500" placeholder="Enter email" />
+                    </div>
+                    <div>
+                        <label class="text-slate-900 text-sm font-medium mb-2 block">Password</label>
+                        <input name="password" type="password" class="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500" placeholder="Enter password" />
+                    </div>
+                    <div>
+                        <label class="text-slate-900 text-sm font-medium mb-2 block">Confirm Password</label>
+                        <input name="cpassword" type="password" class="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500" placeholder="Enter confirm password" />
+                    </div>
+
+                    <div class="flex items-center">
+                        <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
+                        <label for="remember-me" class="text-slate-600 ml-3 block text-sm">
+                            I accept the <a href="javascript:void(0);" class="text-blue-600 font-medium hover:underline ml-1">Terms and Conditions</a>
+                        </label>
+                    </div>
+                </div>
+
+                <div class="mt-12">
+                    <button type="button" class="w-full py-3 px-4 text-sm tracking-wider font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none cursor-pointer">
+                        Create an account
+                    </button>
+                </div>
+                <p class="text-slate-600 text-sm mt-6 text-center">Already have an account? <a href="javascript:void(0);" class="text-blue-600 font-medium hover:underline ml-1">Login here</a></p>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
